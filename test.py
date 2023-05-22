60b64a1466464718e5e3527920fc1b8d179aaa2b
Gabe and Sam <arizeanalytics@gmail.com>
2023-05-22 15:46:05 -0700

    Fixed wrap around no capture bug with first pocket

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def main():
     print(mancala.generateMoves(startState))
     """
     #startState[1] = 2
-    print(mancala.succAndReward(startState, 2))
+    #print(mancala.succAndReward(startState, 2))
 
     # endState = [[0, 0, 0, 0, 0, 0, 10,
     #             2, 2, 2, 2, 2, 2, 0], 2]
@@ -26,6 +26,16 @@ def main():
     #wrapAroundState = [[1, 0, 2, 3, 0, 13, 10,
                        # 2, 2, 2, 2, 2, 12, 0], 2]
     #print(mancala.succAndReward(wrapAroundState, 12))
+
+    fullWrapAroundStatePlusCapture = [[2, 2, 2, 2, 2, 2, 0,
+     0, 0, 0, 0, 0, 13, 0], 2]
+    print(mancala.succAndReward(fullWrapAroundStatePlusCapture, 12))
+
+    # captureState = [[1, 0, 2, 3, 0, 0, 10,
+    # 2, 2, 2, 2, 2, 2, 0], 1]
+    # print(mancala.succAndReward(captureState, 0))
+
+    # Also make sure to test capturing all the way back around to og pocket
 
 
 
